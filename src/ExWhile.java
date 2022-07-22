4e38b796011aa6309abca6daa20d7ce125522210
Heriberto Fernandez <hfernandezaguirre@gmail.com>
2022-07-21 20:56:16 -0500

    Ejercicios while

---
+++ src/ExWhile.java
@@ -0,0 +1,10 @@
+public class ExWhile {
+    public static void main(String[] args) {
+        int numw=0;
+        int numw2=10;
+        while (numw<numw2){
+            numw = numw +1; // numw++;
+            System.out.println(numw);
+        }
+    }
+}
