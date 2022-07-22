7cb218bcbb73a7c3416f43756d353277233e615f
Heriberto Fernandez <hfernandezaguirre@gmail.com>
2022-07-21 20:49:31 -0500

    Ejercicios for

---
+++ src/ExerciseFor.java
@@ -0,0 +1,15 @@
+public class ExerciseFor {
+    public static void main(String[] args) {
+        int num = 1;
+        for (int i=20;num<=i;num++){
+            System.out.println(num);
+        }
+
+        int numD = 20;
+        for (int i=1;numD>=i;numD--){
+            System.out.println(numD);
+        }
+
+    }
+
+}
